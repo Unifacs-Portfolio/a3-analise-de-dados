16ed3e9450a2b8082bc087226e90568d660cb443
Erick Barros Ferreira Gomes <erickgomes.developer@gmail.com>
2026-05-21 20:39:13 -0300

    feat: consolidar base histórica de saúde

--- main.py
+++ main.py
@@ -1,95 +1,119 @@
 import pandas as pd
 import glob
+from functools import reduce
 
+def remove_footer(df):
+    filtro_total = df.iloc[:, 0].astype(str).str.contains('Total', case=False, na=False)
+    if filtro_total.any():
+        df = df.iloc[:filtro_total.idxmax()]
+    return df
 def processar_dados_ano(ano):
     """
     Função que extrai, limpa e consolida os dados de um ano específico.
     """
     print(f'\n--- Iniciando processamento do ano: {ano} ---')
 
-    # ==========================================
-    # 1. CONSOLIDAÇÃO DO DATASUS
-    # ==========================================
     arquivos_estados = glob.glob(f'./datasets/mortalidade_estados_{ano}/*.csv')
     
-    df_list = []
-    for f in arquivos_estados:
-        with open(f, 'r', encoding='latin1') as file:
-            linhas = file.readlines()
-            
-        pulos = 0
-        for i, linha in enumerate(linhas):
-            if "Munic" in linha and ";" in linha: 
-                pulos = i
-                break
-                
-        df_temp = pd.read_csv(f, sep=';', encoding='latin1', skiprows=pulos, skipfooter=2, engine='python')
-        df_list.append(df_temp)
+    lista_dfs = [
+        remove_footer(pd.read_csv(
+            arquivo, 
+            encoding='latin1', 
+            sep=';', 
+            header=3,
+            names=['municipio', 'obitos_por_residencia']
+        )) 
+        for arquivo in arquivos_estados
+    ]
+    df_mort_evit = pd.concat(lista_dfs, ignore_index=True)
+    df_mort_evit[['id_municipio', 'nome_municipio']] = df_mort_evit['municipio'].astype(str).str.split(r'^\s*(\d{6})\s+', expand=True).iloc[:, 1:3]
+    df_mort_evit = df_mort_evit.drop(columns=['municipio'])
 
-    df_mort_evit = pd.concat(df_list, ignore_index=True)
-    df_mort_evit.rename(columns={df_mort_evit.columns[0]: 'id_municipio_str', df_mort_evit.columns[1]: 'total_obitos_evitaveis'}, inplace=True)
-    df_mort_evit['id_chave'] = df_mort_evit['id_municipio_str'].astype(str).str[:6]
-
-    # ==========================================
-    # 2. CARREGAMENTO DAS OUTRAS FONTES
-    # ==========================================
-    # Lendo os arquivos das pastas baseadas no ano
     df_pop = pd.read_csv(f'./datasets/datasets{ano}/populacao_ibge_{ano}.csv')
-    df_pop['id_chave'] = df_pop['id_municipio'].astype(str).str[:6]
+    df_pop['id_municipio'] = df_pop['id_municipio'].astype(str).str[:6]
+    
+    df_ans = remove_footer(pd.read_csv(
+        f'./datasets/datasets{ano}/beneficiarios_{ano}.csv', 
+        encoding='latin1', 
+        sep=';', 
+        header=3, 
+        usecols=[0, 1], 
+        names=['municipio', 'beneficiarios_ans']
+    ))
+    df_ans[['id_municipio', 'nome_municipio']] = df_ans['municipio'].astype(str).str.split(r'^\s*(\d{6})\s+', expand=True).iloc[:, 1:3]
+    df_ans = df_ans.drop(columns=['municipio'])
+    
+    df_pib = pd.read_csv(
+        f'./datasets/datasets{ano}/pib_municipios_ibge_{ano}.csv', 
+        sep=';', 
+        header=3, 
+        decimal=',',
+        names=['id_municipio', 'nome_municipio', 'pib_milhares']
+    ) 
+    df_pib['id_municipio'] = df_pib['id_municipio'].astype(str).str[:6]
+    
+    print(df_mort_evit)
+    df_mort_evit.info()
+    print()
+    
+    print(df_pop)
+    df_pop.info()
+    print()
+    
+    print(df_ans)
+    df_ans.info()
+    print()
+    
+    print(df_pib)
+    df_pib.info()
+    
+    print('\nValores Nulos:')
+    print(df_mort_evit.isna().sum())
+    
+    print('\nValores Nulos:')
+    print(df_pop.isna().sum())
+    
+    print('\nValores Nulos:')
+    print(df_ans.isna().sum())
+    
+    print('\nValores Nulos:')
+    print(df_pib.isna().sum())
 
-    # Lendo ANS
-    df_ans = pd.read_csv(f'./datasets/datasets{ano}/beneficiarios_{ano}.csv', encoding='latin1', sep=';', skiprows=3, skipfooter=2, engine='python')
-    
-    # Pega estritamente as duas primeiras colunas (ignorando colunas extras fantasmas)
-    df_ans = df_ans.iloc[:, :2] 
-    
-    # Agora podemos renomear com segurança
-    df_ans.columns = ['id_municipio_str', 'total_beneficiarios']
-    df_ans['id_chave'] = df_ans['id_municipio_str'].astype(str).str[:6]
-
-    df_pib = pd.read_csv(f'./datasets/datasets{ano}/pib_municipios_ibge_{ano}.csv', sep=';', skiprows=3, skipfooter=10, engine='python') 
-    
-    coluna_valor = df_pib.columns[-1]
-    col_codigo = df_pib.columns[0] if 'Cód' in df_pib.columns[0] else df_pib.columns[1]
-    df_pib['id_chave'] = df_pib[col_codigo].astype(str).str.extract(r'(\d+)')[0].str[:6]
-    
-    df_pib['pib_per_capita'] = pd.to_numeric(
-        df_pib[coluna_valor].astype(str).str.replace('.', '').str.replace(',', '.'), 
-        errors='coerce'
+    fila_de_dados = [
+        df_pop,
+        df_pib[['id_municipio', 'pib_milhares']],
+        df_ans[['id_municipio', 'beneficiarios_ans']],
+        df_mort_evit[['id_municipio', 'obitos_por_residencia']]
+    ]
+    df_consolidado = reduce(
+        lambda left, right: pd.merge(left, right, on='id_municipio', how='left'), 
+        fila_de_dados
     )
-
-    # ==========================================
-    # 3. MERGE (A GRANDE JUNÇÃO)
-    # ==========================================
-    df_consol = pd.merge(df_pop, df_mort_evit[['id_chave', 'total_obitos_evitaveis']], on='id_chave', how='left')
-    df_consol = pd.merge(df_consol, df_ans[['id_chave', 'total_beneficiarios']], on='id_chave', how='left')
-    df_consol = pd.merge(df_consol, df_pib[['id_chave', 'pib_per_capita']], on='id_chave', how='left')
-
-    # ==========================================
-    # 4. ENGENHARIA DE FEATURES
-    # ==========================================
-    df_consol['total_obitos_evitaveis'] = df_consol['total_obitos_evitaveis'].fillna(0)
-    df_consol['total_beneficiarios'] = df_consol['total_beneficiarios'].fillna(0)
-
-    df_consol['taxa_mortalidade_evitavel'] = (df_consol['total_obitos_evitaveis'] / df_consol['populacao']) * 100000
-    df_consol['perc_cobertura_saude'] = (df_consol['total_beneficiarios'] / df_consol['populacao']) * 100
+    print(df_consolidado)
+    df_consolidado.info()
+    df_consolidado['beneficiarios_ans'] = df_consolidado['beneficiarios_ans'].fillna(0).astype(int)
+    df_consolidado['obitos_por_residencia'] = df_consolidado['obitos_por_residencia'].fillna(0).astype(int)
+    df_consolidado.info()
+    print("\n--- Verificando o Município sem PIB ---")
+    print(df_consolidado[df_consolidado['pib_milhares'].isna()])
+    df_consolidado = df_consolidado.dropna(subset=['pib_milhares'])
     
-    # CRIANDO A COLUNA DO ANO (Crucial para o EDA)
-    df_consol['ano'] = ano
+    print(f"Total de municípios consistentes: {len(df_consolidado)}")
     
-    return df_consol
+    df_consolidado['taxa_mortalidade_evitavel'] = (df_consolidado['obitos_por_residencia'] / df_consolidado['populacao']) * 100000
+    df_consolidado['perc_cobertura_saude'] = (df_consolidado['beneficiarios_ans'] / df_consolidado['populacao']) * 100
+    df_consolidado['ano'] = int(ano)
+    print(df_consolidado)
+    df_consolidado.info()
+    print(f"\n--- ANO {ano} CONSOLIDADO COM SUCESSO! ---")
+    return df_consolidado
 
-# ==========================================
-# EXECUÇÃO PRINCIPAL DO PIPELINE
-# ==========================================
 if __name__ == "__main__":
     print('Iniciando pipeline de dados histórico...')
     
-    # Lista de anos que queremos analisar
     anos_analise = ['2018', '2020', '2022']
     bases_anuais = []
     
-    # Roda a nossa função para cada ano e guarda o resultado
     for ano in anos_analise:
         base_do_ano = processar_dados_ano(ano)
         bases_anuais.append(base_do_ano)
@@ -97,8 +121,10 @@ if __name__ == "__main__":
     print('\nEmpilhando todos os anos em um dataset mestre...')
     df_historico = pd.concat(bases_anuais, ignore_index=True)
     
-    # Salva a super base histórica
     caminho_saida = './datasets/base_consolidada_saude_historico.csv'
+    print(df_historico.tail(10))
+    df_historico.info()
+    print(df_historico.describe(include='all'))
     df_historico.to_csv(caminho_saida, index=False)
     
     print(f'Pipeline finalizada com sucesso! Base histórica salva em: {caminho_saida}')
