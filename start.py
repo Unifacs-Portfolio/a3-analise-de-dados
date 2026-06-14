a68b07fb5e344c0c2fbbc8a4593a993692c885b8
Roan Nascimento Lisboa <roanlisboa4@gmail.com>
2026-06-14 17:03:59 -0300

    refactor: Alteração para a sequencia correta de inicialização do script inicialização do projeto.

--- start.py
+++ start.py
@@ -58,7 +58,6 @@ def main():
     try:
         subprocess.run([python_venv, "main.py"])
         subprocess.run([python_venv, "etlv2.py"])
-        subprocess.run([python_venv, "eda.py"])
     except KeyboardInterrupt:
         print("\n🛑 Aplicação encerrada pelo usuário.")
 
